43fe02bfc8191a1249e1e1eb979dc79f5a55b532
Tanner Welsh <tannerwelsh@gmail.com>
2020-04-08 18:42:34 -0600

    build: Setup example site w/ custom config
    Showing near config options

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  plugins: [{ resolve: `gatsby-theme-near`, options: {} }],
-}
+  plugins: [
+    {
+      resolve: "gatsby-theme-near",
+      options: {
+        nearConfig: {
+          // Example custom options to pass to gatsby-theme-near
+          networkId: "default",
+          nodeUrl: "https://rpc.nearprotocol.com",
+          walletUrl: "https://wallet.nearprotocol.com",
+          helperUrl: "https://near-contract-helper.onrender.com",
+        },
+      },
+    },
+  ],
+};
