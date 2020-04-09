15cac125357b65aec0054df0ed6b5b63f0f04110
Tanner Welsh <tannerwelsh@gmail.com>
2020-04-08 18:43:21 -0600

    chore: Use NearAuthenticate in example homepage

--- example/src/pages/index.js
+++ example/src/pages/index.js
@@ -1,3 +1,11 @@
-import React from "react"
+import React from "react";
 
-export default () => <div>Homepage in a user's site</div>
+import Layout from "gatsby-theme-near/src/components/layout";
+import NearAuthenticate from "gatsby-theme-near/src/components/near-authenticate"
+
+export default () => (
+  <Layout>
+    <NearAuthenticate signInParams={{ title: "Example App" }} />
+    Homepage in a user's site
+  </Layout>
+);
