d02f2bd9799fa0a41e834b1600171b816dea4b23
Tanner Welsh <tannerwelsh@gmail.com>
2020-04-10 09:09:30 -0600

    chore: Show use of Text component in example

--- example/src/pages/index.js
+++ example/src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Text } from "theme-ui"
 
 import Layout from "gatsby-theme-near/src/components/layout";
 import NearAuthenticate from "gatsby-theme-near/src/components/near-authenticate"
@@ -7,5 +8,9 @@ export default () => (
   <Layout>
     <NearAuthenticate signInParams={{ title: "Example App" }} />
     Homepage in a user's site
+    <Text variant="code">
+      let status = await connection.provider.status();
+      const blockHash = status.sync_status.last_block_hash;
+    </Text>
   </Layout>
 );
